026955401d80723f1f6b5a1ed1715bbf87d7b437
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-02-26 22:23:10 +0530

    Adds jwt auth middleware to blogs endpoint

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,13 +1,33 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
 
 const bookRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
+
+bookRouter.use(async (c, next) => {
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ Error: "Unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set("userId", payload.id);
+  await next();
+});
 
 bookRouter.get("/:id", (c) => {
   const id = c.req.param("id");
