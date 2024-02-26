d4c21aa09686b417db22fac30fc0ca1ce645f334
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-02-26 16:21:42 +0530

    Adds signup route with hashing and jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,37 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { hashFunction } from "./functions/hash";
+import { sign } from "hono/jwt";
 
-// Create the main Hono app
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post("/api/v1/signup",async (c) => {
-  return c.text("signup route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const hashedPass = await hashFunction(body.password);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hashedPass,
+        name: body.name,
+      },
+    });
+    const jwtToken = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwtToken });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Error while signing up." });
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
