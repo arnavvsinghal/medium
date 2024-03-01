59e64d16c650813c8024641bf23f03ef66883a84
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-01 14:07:09 +0530

    Updates the way of jwt auth in blogs route

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { verify } from "hono/jwt";
+import { jwt } from "hono/jwt";
+
 import Bindings from "../bindings";
 import { createPostInput,updatePostInput } from "@arnavitis/medium-common";
+
 const blogRouter = new Hono<{
   Bindings: Bindings;
   Variables: {
@@ -11,21 +13,14 @@ const blogRouter = new Hono<{
   };
 }>();
 
-blogRouter.use(async (c,next) => {
-  const jwt = c.req.header("Authorization");
-  if (!jwt) {
-    c.status(401);
-    return c.json({ error: "Unauthorized" });
+blogRouter.use(
+  (c, next) => {
+    const jwtMiddleware = jwt({
+      secret: c.env.JWT_SECRET,
+    })
+    return jwtMiddleware(c, next)
   }
-  const token = jwt.split(" ")[1];
-  const payload = await verify(token, c.env.JWT_SECRET);
-  if (!payload) {
-    c.status(401);
-    return c.json({ error: "Unauthorized" });
-  }
-  c.set("userId", payload.id);
-  await next();
-});
+)
 
 blogRouter.get("/:id", async (c) => {
   const id = c.req.param("id");
@@ -40,11 +35,10 @@ blogRouter.get("/:id", async (c) => {
 });
 
 blogRouter.post("/", async (c) => {
-  const userId = c.get("userId");
+  const userId = c.get("jwtPayload").id;
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-
   const body = await c.req.json();
 
   const {success} = createPostInput.safeParse(body);
@@ -65,7 +59,7 @@ blogRouter.post("/", async (c) => {
 });
 
 blogRouter.put("/", async (c) => {
-  const userId = c.get("userId");
+  const userId = c.get("jwtPayload").id;
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
