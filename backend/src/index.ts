6d14582e056594554c42cc3da473a7075a449c3b
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-03 22:27:39 +0530

    Adds cors to the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import userRouter from "./routes/user";
 import blogRouter from "./routes/blog";
 import {Bindings} from "./context";
+import { cors } from 'hono/cors'
 
 export const app = new Hono<{
   Bindings: Bindings;
 }>();
 
+app.use('/*', cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
