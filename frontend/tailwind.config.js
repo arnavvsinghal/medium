68e9b3faf0f98b9f65154636408817dd75047622
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-03 17:17:30 +0530

    Updates dependencies and configuration files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -66,6 +66,9 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -90,7 +93,7 @@ module.exports = {
   plugins: [require("tailwindcss-animate"),addVariablesForColors],
 }
 
-function addVariablesForColors({ addBase, theme }: any) {
+function addVariablesForColors({ addBase, theme }) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
