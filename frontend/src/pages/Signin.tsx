bdcaa1c7ddb145e1894d03a2f870762992220826
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-04 19:32:09 +0530

    Improves spacing and padding of pages

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,7 +1,6 @@
 import SignInForm from "@/components/ui/signin-form";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import { BackgroundBeams } from "@/components/ui/background-beams";
-
 import { FunctionComponent } from "react";
 interface SignInProps {}
 const SignIn: FunctionComponent<SignInProps> = () => {
@@ -11,12 +10,12 @@ const SignIn: FunctionComponent<SignInProps> = () => {
     }
   ];
   return (
-    <div className="h-screen flex justify-evenly items-center bg-bgmain">
+    <div className="h-screen flex justify-evenly items-center bg-bgmain px-4">
       <BackgroundBeams/>
       <div className="w-1/2 hidden md:block">
         <TypewriterEffectSmooth words={words} />
       </div>
-      <div className="bg-main">
+      <div className="bg-main z-20">
         <SignInForm />
       </div>
     </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -11,12 +11,12 @@ const SignUp: FunctionComponent<SignUpProps> = () => {
         }
       ];
   return (
-    <div className="h-screen flex justify-evenly items-center bg-bgmain">
+    <div className="h-screen flex justify-evenly items-center bg-bgmain px-4">
       <BackgroundBeams/>
       <div className="w-1/2 hidden md:block">
         <TypewriterEffectSmooth words={words} />
       </div>
-      <div className="bg-main">
+      <div className="bg-main z-20">
         <SignUpForm />
       </div>
     </div>
