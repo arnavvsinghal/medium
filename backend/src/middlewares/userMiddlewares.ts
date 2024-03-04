aa0d06cdf38ae408284df5e3fc118ed177423246
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-04 18:16:03 +0530

    Fixes wrong messages sent

--- backend/src/middlewares/userMiddlewares.ts
+++ backend/src/middlewares/userMiddlewares.ts
@@ -1,24 +1,24 @@
 import type { MiddlewareHandler } from "hono";
-import { signinInput,signupInput } from "@arnavitis/medium-common";
+import { signinInput, signupInput } from "@arnavitis/medium-common";
 
 export const userSignupValidation: MiddlewareHandler = async (c, next) => {
   const body = await c.req.json();
   const res = signupInput.safeParse(body);
-  if(!res.success){
+  if (!res.success) {
     c.status(400);
-    return c.json({ error: res.error.message });
-  }  
-  c.set("body",body);
+    return c.json({ error: res.error.issues[0].message });
+  }
+  c.set("body", body);
   await next();
 };
 
 export const userSigninValidation: MiddlewareHandler = async (c, next) => {
   const body = await c.req.json();
   const res = signinInput.safeParse(body);
-  if(!res.success){
+  if (!res.success) {
     c.status(400);
-    return c.json({ error: res.error.message });
-  }  
-  c.set("body",body);
+    return c.json({ error: res.error.issues[0].message });
+  }
+  c.set("body", body);
   await next();
 };
