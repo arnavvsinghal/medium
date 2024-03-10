3b16e3f7e7894ff85bce03dd5040adbd2d1cd51b
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-10 12:14:57 +0530

    Encapsulates redirection from Landing, SignIn and SignUp page in a custom hook

---
+++ frontend/src/hooks/useCheckSignIn.ts
@@ -0,0 +1,27 @@
+import { BACKEND_URL } from "@/config";
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useCheckSignIn = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+      const useEffectAsync = async () => {
+        if (localStorage.token) {
+          try {
+            await axios.get(`${BACKEND_URL}/api/v1/user/me`, {
+              headers: {
+                Authorization: `Bearer ${localStorage.token}`,
+              },
+            });
+            navigate("/blogs")
+          } catch (err) {
+            localStorage.removeItem("token");
+            navigate("/");
+          }
+        }
+      };
+      useEffectAsync();
+    }, [navigate]);
+}
+export default useCheckSignIn;
