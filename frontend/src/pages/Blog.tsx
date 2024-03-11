6400df0d190f492c1333aa113693f980e9fec804
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-03-11 21:26:38 +0530

    Adds better error detection as well as directing

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,36 +2,40 @@ import AppBar from "@/components/ui/appbar";
 import AvatarImg from "@/components/ui/avatar";
 import { Heading } from "@/components/ui/heading";
 import { Skeleton } from "@/components/ui/skeleton";
+import userAtom from "@/store/atom/user";
 import specificBlogSelector from "@/store/selectorFamily/specificBlogSelector";
-
-import { FunctionComponent, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useRecoilValueLoadable } from "recoil";
-interface BlogProps {}
-interface CardProps {
-  id: string;
-  author: {
-    id: string;
-    name: string;
-  };
-  date: string;
-  title: string;
-  content: string;
-}
-const Blog: FunctionComponent<BlogProps> = () => {
+const Blog = () => {
+  const userData = useRecoilValueLoadable(userAtom);
   const navigate = useNavigate();
   const [searchParams, _] = useSearchParams();
   const id = searchParams.get("id") ?? "";
   const blog = useRecoilValueLoadable(specificBlogSelector(id));
+  const blogExists: Boolean =
+    blog.state === "hasValue" && blog.contents.length ? true : false;
   useEffect(() => {
-    if (blog.state === "hasError" || (blog.state === "hasValue" && !blog.contents.length)) {
+    if (
+      blog.state === "hasError" ||
+      (blog.state === "hasValue" && blog.contents.length == 0) ||
+      userData.state === "hasError"
+    ) {
       navigate("/blogs");
     }
   }, [blog, navigate]);
-
   return (
     <div className="flex flex-col items-center bg-bgmain min-h-screen">
-      <AppBar variant="post" />
+      <AppBar
+        variant={
+          userData.state == "hasValue" &&
+          blogExists &&
+          userData.contents.id == blog.contents[0].author.id
+            ? "edit"
+            : "post"
+        }
+        blogId={blogExists ? blog.contents[0].id : null}
+      />
       {blog.state == "loading" ? (
         <div className="flex flex-col items-center w-screen flex-grow">
           <Skeleton className="h-40 w-40 mt-4 mb-2 rounded-full" />
@@ -42,14 +46,19 @@ const Blog: FunctionComponent<BlogProps> = () => {
       ) : (
         <div className="flex flex-col items-center w-3/4 mx-auto">
           <div className="h-40 w-40 mt-4 mb-2">
-            <AvatarImg shape="circle" id={blog.contents[0].id} />
+            <AvatarImg
+              shape="circle"
+              id={blogExists ? blog.contents[0].author.id : ""}
+            />
           </div>
-          <Heading className="text-5xl">{blog.contents[0].author.name}</Heading>
+          <Heading className="text-5xl">
+            {blogExists ? blog.contents[0].author.name : ""}
+          </Heading>
           <div className="text-3xl text-textsecondary mt-4 mb-2">
-            {blog.contents[0].title}
+            {blogExists ? blog.contents[0].title : ""}
           </div>
           <div className="text-lg text-textsecondary mt-4 mb-2">
-            {blog.contents[0].content}
+            {blogExists ? blog.contents[0].content : ""}
           </div>
         </div>
       )}
