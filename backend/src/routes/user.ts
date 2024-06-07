582a7ae8bea3a846786a06bb1c45a58837c418e4
Arnav <arnavsinghaldev@gmail.com>
2024-06-07 18:24:57 +0530

    chore: change workflows and add wrangler

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -16,7 +16,7 @@ const userRouter = new Hono<userContext>();
 userRouter.get("/me", jwtAuth, async (c) => {
   const userId = c.get("jwtPayload").id;
   return c.json({
-    id: "CI CD CHECK",
+    id: "Does it work?",
   });
 });
 userRouter.get("/info", jwtAuth, async (c) => {
