ce5d3d135a2af1833b32bf0707235ff2e1930e81
Arnav <arnavsinghaldev@gmail.com>
2024-06-07 16:39:17 +0530

    chore: add comment to check ci

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 //Random Comment to check CI
+//Comment Two
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
