d744b2ed0bc05be25c36fd29a877144df1af535c
Arnav <arnavsinghaldev@gmail.com>
2024-06-07 18:38:07 +0530

    ci/cd: :construction_worker: changing comments to check ci/cd

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -16,7 +16,7 @@ const userRouter = new Hono<userContext>();
 userRouter.get("/me", jwtAuth, async (c) => {
   const userId = c.get("jwtPayload").id;
   return c.json({
-    id: "Does it work?",
+    id: "Changing it again?",
   });
 });
 userRouter.get("/info", jwtAuth, async (c) => {
