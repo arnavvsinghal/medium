dc5bd4661924140dd26eb2fe122f2b37fad7a23e
Arnav <arnavsinghaldev@gmail.com>
2024-06-07 19:34:07 +0530

    ci/cd: comment change

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -16,7 +16,7 @@ const userRouter = new Hono<userContext>();
 userRouter.get("/me", jwtAuth, async (c) => {
   const userId = c.get("jwtPayload").id;
   return c.json({
-    id: "This should work no",
+    id: "This should work n",
   });
 });
 userRouter.get("/info", jwtAuth, async (c) => {
