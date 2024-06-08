b11ae0b808bcbd5ed717ede535bf189640309204
Arnav <arnavsinghaldev@gmail.com>
2024-06-08 12:53:52 +0530

    ci/cd(backend): check workflow

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -16,7 +16,7 @@ const userRouter = new Hono<userContext>();
 userRouter.get("/me", jwtAuth, async (c) => {
   const userId = c.get("jwtPayload").id;
   return c.json({
-    id: "userId",
+    id: "userI",
   });
 });
 userRouter.get("/info", jwtAuth, async (c) => {
